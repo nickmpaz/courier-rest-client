75047970c61275dc890ceab04d13e838951c079e
Nick Paz <nicholasmpaz@gmail.com>
2022-11-12 15:16:50 -0800

    Make request payload editable

--- src/providers/RequestProvider.ts
+++ src/providers/RequestProvider.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import fetch from "node-fetch";
-import { RequestFunction } from "../definitions/types";
+import { Request, RequestFunction } from "../definitions/types";
 import { getWebviewHtml } from "../definitions/helpers";
 import { RestClient } from "../RestClient";
 import { RequestStatus } from "./ResponseProvider";
@@ -9,6 +9,7 @@ import { commands } from "../definitions/constants";
 class RequestProvider implements vscode.WebviewViewProvider {
   private view?: vscode.WebviewView;
   private requestFunction?: RequestFunction;
+  private requestPayload?: Request;
   private restClient: RestClient;
 
   constructor(restClient: RestClient) {
@@ -36,39 +37,50 @@ class RequestProvider implements vscode.WebviewViewProvider {
       switch (message.command) {
         case "send-request":
           this.sendRequest();
+          break;
+        case "reset-request":
+          this.update(this.requestFunction);
+          break;
+        case "update-request-property":
+          if (this.requestPayload) {
+            this.requestPayload = {
+              ...this.requestPayload,
+              [message.key]: message.value,
+            };
+          }
+          break;
       }
     });
-    this.update()
+    this.update();
   }
   async sendRequest() {
-    if (this.requestFunction === undefined) {
+    if (this.requestPayload === undefined) {
       return;
     }
-
     this.restClient.responseProvider.update(undefined, RequestStatus.Loading);
-    const env = await this.restClient.environmentProvider.loadEnvironment();
-    const request = this.requestFunction(env);
-    const response = await fetch(request.url, request);
+    const response = await fetch(this.requestPayload.url, this.requestPayload);
     this.restClient.responseProvider.update(
       await response.text(),
       RequestStatus.Success
     );
   }
 
-  update(requestFunction?: RequestFunction) {
+  async update(requestFunction?: RequestFunction) {
     this.requestFunction = requestFunction;
-    if (!this.view) {
+    if (!this.requestFunction || !this.view) {
       return;
     }
+    const env = await this.restClient.environmentProvider.loadEnvironment();
+    this.requestPayload = this.requestFunction(env);
+    this.view.webview.html = "";
     this.view.webview.html = this.getHtml(
       this.view.webview,
-      this.requestFunction?.toString() ?? "Select a request",
-      !!this.requestFunction
+      this.requestPayload ?? { url: "none" },
+      !!this.requestPayload
     );
   }
 
-  getHtml(webview: vscode.Webview, content: string, isCode: boolean) {
-    const contentTag = isCode ? "pre" : "div";
+  getHtml(webview: vscode.Webview, request: Request, isCode: boolean) {
     return getWebviewHtml(
       this.restClient.context,
       webview,
@@ -77,16 +89,23 @@ class RequestProvider implements vscode.WebviewViewProvider {
       ["request.css"],
       /* html */ `
       <div class="content-container full-height">
-      <div class="actions">
-        <vscode-button id="send-request" appearance="primary">Send</vscode-button>
-      </div>
-      <div class="code-display-container full-height flex-grow">
-        <${contentTag} class="code-display full-height">${content}
-        </${contentTag}>
-      </div>
+        <div class="actions">
+          <vscode-button id="reset-request" appearance="secondary">Reset</vscode-button>
+          <vscode-button id="send-request" appearance="primary">Send</vscode-button>
+        </div>
+        ${Object.entries(request)
+          .map(
+            ([key, value]) => /* html */ `
+              <vscode-text-field value="${value}" class="request-property-input" data-request-property="${key}">
+                ${key}
+              </vscode-text-field>`
+          )
+          .join(" ")}
+
       </div>`
     );
   }
 }
 
 export { RequestProvider };
+  
--- webview-ui/request.js
+++ webview-ui/request.js
@@ -8,4 +8,23 @@ function main() {
       command: "send-request",
     });
   });
+
+  document.getElementById("reset-request").addEventListener("click", () => {
+    vscode.postMessage({
+      command: "reset-request",
+    });
+  });
+
+  document.querySelectorAll(".request-property-input").forEach((el) => {
+    el.addEventListener("input", function (evt) {
+      const value = this.value;
+      const key = el.getAttribute("data-request-property");
+      console.log({ key, value });
+      vscode.postMessage({
+        command: "update-request-property",
+        key,
+        value,
+      });
+    });
+  });
 }
